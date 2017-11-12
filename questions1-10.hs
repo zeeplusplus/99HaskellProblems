8ae9c6129b32f28aa47ccd1a2078442b9194638e
Alex Zeeplusplus <sojoverezy@gmail.com>
2017-11-12 18:33:13 +0100

    Solution for problem 3

--- questions1-10.hs
+++ questions1-10.hs
@@ -1,12 +1,20 @@
--- Problem 1: find the last element of a list
+-- Problem 1: Find the last element of a list
 myLast :: [a] -> a
 myLast [x] = x
 myLast (_:xs) = head (reverse xs)
 myLast []     = error "myLast: empty list"
 
--- Problem 2: find the last but one element of a list
+-- Problem 2: Find the last but one element of a list
 myButLast :: [a] -> a
-myButLast []       = error "myLast: too few elements"
-myButLast [x]      = error "myLast: too few elements"
+myButLast []       = error "myButLast: too few elements"
+myButLast [x]      = error "myButLast: too few elements"
 myButLast [x, _]   = x
 myButLast (_:xs)   = myButLast xs
+
+-- Problem 3: Find the K'th element of a list. The first element in the list is number 1
+elementAt :: (Integral b) => [a] -> b -> a
+elementAt [] _ = error "elementAt: invalid input"
+elementAt (x:xs) n
+    | length xs < (fromIntegral(n) - 1) = error "elementAt: invalid input"
+    | n == 1    = x
+    | otherwise = elementAt xs (n - 1)
